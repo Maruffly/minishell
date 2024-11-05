843f779b7917969fedf0ba55b0880cb33dd46fea
Joris Laine <jlaine@paul-f5Br3s2.clusters.42paris.fr>
2024-11-05 15:07:48 +0100

    Merged master into Joris

---
+++ Libft/ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaruffy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 17:13:59 by jmaruffy          #+#    #+#             */
+/*   Updated: 2024/05/22 14:49:43 by jmaruffy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t length)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < length)
+	{
+		if ((unsigned char)s1[i] < (unsigned char)s2[i])
+			return (-1);
+		if ((unsigned char)s1[i] > (unsigned char)s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}

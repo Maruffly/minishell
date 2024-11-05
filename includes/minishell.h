6e65d8f139c243221c1402af94dc80033bfc97ef
Joris Laine <jlaine@paul-f5Br3s2.clusters.42paris.fr>
2024-11-05 15:44:37 +0100

    add colors to header

--- includes/minishell.h
+++ includes/minishell.h
@@ -8,8 +8,16 @@
 # include <sys/wait.h>
 # include <unistd.h>
 # include <string.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// Macros
+# define RESET	"\e[0m"
+# define RED	"\e[31m"
+# define GREEN	"\e[32m"
+# define YELL	"\e[33m"
+# define PURPLE	"\e[35m"
+# define CYAN	"\e[36m"
 
 void	read_line(void);
 

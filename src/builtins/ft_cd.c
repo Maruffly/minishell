29d3eba84d24edcbf160df57c401963ebdd908ef
Jean-Baptiste Maruffy <jmaruffy@paul-f5Br3s1.clusters.42paris.fr>
2024-11-07 18:07:15 +0100

    test worklow norminette

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: jmaruffy <jmaruffy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:55:10 by jmaruffy          #+#    #+#             */
-/*   Updated: 2024/11/07 17:24:29 by jmaruffy         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:06:18 by jmaruffy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,29 @@
 void	cd_builtin()
 {
 
+}
+
+void minishell_cd(char **args) {
+    // Vérifier que l'utilisateur a fourni un argument
+    if (!args[1]) {
+        fprintf(stderr, "cd: missing argument\n");
+        return;
+    }
+
+    // Vérifier si le chemin est absolu ou relatif
+    if (args[1][0] == '/' || strncmp(args[1], "./", 2) == 0 || strncmp(args[1], "../", 3) == 0 || strcmp(args[1], ".") == 0 || strcmp(args[1], "..") == 0) {
+        // Tenter de changer de répertoire
+        if (chdir(args[1]) == -1) {
+            // Si chdir échoue, afficher une erreur
+            fprintf(stderr, "cd: %s: %s\n", args[1], strerror(errno));
+        }
+    } else {
+        fprintf(stderr, "cd: only relative or absolute paths are allowed\n");
+    }
+}
+
+int main(int argc, char **argv) {
+    // Exemple d'utilisation : ./minishell_cd /home ou ./minishell_cd ../..
+    minishell_cd(argv);
+    return 0;
 }

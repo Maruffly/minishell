0fd1652829903a2a9f7b44e8a6a1f5587c5991fe
Jean-Baptiste Maruffy <jmaruffy@paul-f4Br3s2.clusters.42paris.fr>
2024-11-11 15:43:22 +0100

    CD + PWD

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,19 +6,25 @@
 /*   By: jmaruffy <jmaruffy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:55:48 by jmaruffy          #+#    #+#             */
-/*   Updated: 2024/11/07 15:27:44 by jmaruffy         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:33:55 by jmaruffy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef BUILTINS_H
 # define BUILTINS_H
 
-// CD //
 # include	<stdio.h>
 # include	<stdlib.h>
 # include	<unistd.h>
 # include	<string.h>
 # include	<errno.h>
+
+// CD //
+
+void	exec_cd(t_command *cmd);
+void	update_pwd_env(t_env_list	*env_list);
+
+// PWD //
 
 
 #endif
--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,13 +6,13 @@
 /*   By: jmaruffy <jmaruffy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:55:10 by jmaruffy          #+#    #+#             */
-/*   Updated: 2024/11/08 15:14:22 by jmaruffy         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:38:16 by jmaruffy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include	"../includes/minishell.h"
 
-void	exec_cd(t_command *cmd)
+void	exec_cd(t_command *cmd, t_env_list *env_list)
 {
 	char	*path;
 
@@ -27,6 +27,23 @@ void	exec_cd(t_command *cmd)
 	if (chdir(path) == -1)
 		perror("path does not exist");
 	else
-		update_pwd_env();
+		update_pwd_env(env_list);
 }
 
+void	update_pwd_env(t_env_list	*env_list)
+{
+	char	*pwd;
+	char	*oldpwd;
+
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		perror("getcwd");
+		return ;
+	}
+	oldpwd = getenv("PWD");
+	if (oldpwd)
+		update_env_var(env_list, "OLDPWD", oldpwd);
+	update_env_var(env_list, "PWD", pwd);
+	free(pwd);
+}

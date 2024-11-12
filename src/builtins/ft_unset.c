97589b2a5be84a04417f1c884e6ee82f5942e10c
Jean-Baptiste Maruffy <jmaruffy@paul-f5Ar2s2.clusters.42paris.fr>
2024-11-12 17:25:10 +0100

    joris - token/command ok + fixed unset

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: jmaruffy <jmaruffy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:28:12 by jmaruffy          #+#    #+#             */
-/*   Updated: 2024/11/12 16:50:38 by jmaruffy         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:17:15 by jmaruffy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,22 @@ void	exec_unset(t_env_list *env_list, t_command *cmd)
 	char	**args;
 	int		i;
 
-	if (!cmd->right)
+	if (!cmd->right || !cmd->right->value)
 		return ;
 	args = ft_split(cmd->right->value, ' ');
+	if (!args)
+		return ;
 	i = 0;
 	while (args[i])
 	{
-		remove_env_node(env_list, args[i]);
+		if (find_env_node(env_list, args[i]))
+			remove_env_node(env_list, args[i]);
+		else
+		{
+			ft_putstr_fd("unset: '", 2);
+			ft_putstr_fd(args[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+		}
 		i++;
 	}
 	ft_free_split(args);

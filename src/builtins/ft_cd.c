58009151da0873068239fbf59f9f402ef98df02d
Joris Laine <jorislaine@MacBook-Air-de-Joris.local>
2025-01-26 17:48:12 +0100

    modif ft_cd shorter version

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbmy <jbmy@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: jorislaine <jorislaine@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 14:55:10 by jmaruffy          #+#    #+#             */
-/*   Updated: 2025/01/20 13:06:35 by jbmy             ###   ########.fr       */
+/*   Updated: 2025/01/26 17:46:40 by jorislaine       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../includes/minishell.h"
 
+/*
 void	exec_cd(t_ast_command *cmd, t_env_list *env_list)
 {
 	char		*path;
@@ -65,6 +66,8 @@ void	exec_cd(t_ast_command *cmd, t_env_list *env_list)
 	update_pwd_env(env_list, cur_pwd);
 	free(cur_pwd);
 }
+*/
+
 
 void	update_pwd_env(t_env_list *env_list, char *old_pwd)
 {
@@ -80,4 +83,80 @@ void	update_pwd_env(t_env_list *env_list, char *old_pwd)
 		update_env_node(env_list, "OLDPWD", old_pwd);
 	update_env_node(env_list, "PWD", new_pwd);
 	free(new_pwd);
+}
+
+
+static void handle_cd_error(char *path, char *cur_pwd)
+{
+	ft_putstr_fd("Omar&Fred: cd: ", 2);
+	perror(path);
+	free(cur_pwd);
+}
+
+static char *get_cd_path(t_ast_command *cmd, t_env_list *env_list)
+{
+	char *path;
+	t_env_list *home_node;
+
+	if (!cmd->args[1])
+	{
+		home_node = find_env_node(env_list, "HOME");
+		if (!home_node || !home_node->var_value)
+		{
+			ft_putstr_fd("cd: Home not set", 2);
+			return (NULL);
+		}
+		path = home_node->var_value;
+	}
+	else
+		path = cmd->args[1];
+	return (path);
+}
+
+static int change_directory(char *path, char *cur_pwd, t_env_list *env_list)
+{
+	if (access(path, F_OK) < 0)
+	{
+		handle_cd_error(path, cur_pwd);
+		return (EXIT_FAILURE);
+	}
+	if (chdir(path) < 0)
+    {
+		handle_cd_error(path, cur_pwd);
+		return (EXIT_FAILURE);
+	}
+	update_pwd_env(env_list, cur_pwd);
+	free(cur_pwd);
+	return (EXIT_SUCCESS);
+}
+
+void exec_cd(t_ast_command *cmd, t_env_list *env_list)
+{
+    char *path;
+    char *cur_pwd;
+
+    if (!cmd->args || !cmd->args[0])
+    {
+        ft_putstr_fd("cd: No command\n", 2);
+        return;
+    }
+    cur_pwd = getcwd(NULL, 0);
+    if (!cur_pwd)
+    {
+        perror("cd");
+        return;
+    }
+    if (cmd->args[2] != NULL)
+    {
+        ft_putstr_fd("cd: Too many arguments\n", 2);
+        free(cur_pwd);
+        return;
+    }
+    path = get_cd_path(cmd, env_list);
+    if (!path)
+    {
+        free(cur_pwd);
+        return;
+    }
+    change_directory(path, cur_pwd, env_list);
 }
